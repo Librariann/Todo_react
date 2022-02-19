ef07d43ca4e747512f0b1e92fefefd9981151709
Librariann <okpc0306@naver.com>
2022-02-20 02:02:24 +0900

    edit todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,74 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import styled from 'styled-components';
 import GlobalStyle from './styled-components/globalstyle';
 
 const Div = styled.div`
-  width:300px;
-  height:100%;
-  margin:0 auto;
-  background-color:blueviolet;
+  width: 300px;
+  height: 100%;
+  margin: 0 auto;
+  background-color: blueviolet;
 `;
 
 const H1 = styled.h1`
-  text-align:center
+  text-align: center;
 `;
 
 const Input = styled.input`
-  width:250px;
+  width: 250px;
 `;
 
 const InputBtn = styled.input`
-  width:48px;
+  width: 48px;
 `;
-interface Todo {
-  id: number; 
-  text: string; 
+interface ITodo {
+  id: number;
+  text: string;
 }
 
 function App() {
-  const [todo, setTodo] = useState<Todo[]>([]);
-  const [value, setValue] = useState('');
-  const [currentValue, setCurrentValue] = useState<string | null>(() =>
-      localStorage.getItem("todos")
-    );
+  const [todo, setTodo] = useState<ITodo[]>([]);
+  const [value, setValue] = useState<string>('');
 
-  
-  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
-      currentTarget:{ value }
-    } = event
+      currentTarget: { value }
+    } = event;
     setValue(value);
-    console.log(value);
-  }
+  };
 
-  const onClick = (event:React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setTodo([
-      ...todo,
-      {
-        id:todo.length+1,
-        text: value
-      }
+  const onClick = () => {
+    const addTodo = {
+      id: todo.length + 1,
+      text: value
+    };
+    setValue('');
+    setTodo(todo.concat(addTodo));
+    localStorage.setItem('todos', JSON.stringify(todo.concat(addTodo)));
+  };
 
-    ])
-    localStorage.setItem("todos",JSON.stringify(todo));
+  const enterHandle = (event: React.KeyboardEvent) => {
+    if (event.keyCode === 13) {
+      onClick();
+    }
+  };
 
-    console.log(todo);
-  }
+  const setTodos = useCallback(() => {
+    setTodo([...JSON.parse(localStorage.getItem('todos') || '')]);
+  }, []);
+
   useEffect(() => {
-    setTodo([
-      JSON.parse(currentValue)
-    ])
-  });
-
-
-
+    setTodos();
+  }, [setTodos]);
 
   return (
-    
     <>
       <GlobalStyle />
       <Div>
         <H1>ToDo-List</H1>
-        <form>
-          <Input 
-            onChange={onChange}
-            type='text'
-          />
-
-          <InputBtn 
-            type='button'
-            value='확인'
-            onClick={onClick}
-          />
-        </form>
+        <Input onChange={onChange} onKeyDown={enterHandle} value={value} type="text" />
+        <InputBtn type="button" value="확인" onClick={onClick} />
         {todo.map((val, idx) => {
-            return <Div>{val.text}</Div>
+          return <Div key={idx}>{val.text}</Div>;
         })}
       </Div>
     </>
